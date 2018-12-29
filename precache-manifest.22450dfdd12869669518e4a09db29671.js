ec618e88b6f1a73fab7d18417531813be7a567c1
Yong Lee <developer@yonglee.ca>
2018-12-28 21:45:54 -0500

    Updates

---
+++ precache-manifest.22450dfdd12869669518e4a09db29671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dda3e6ef8e85d353dfe2",
+    "url": "/static/js/main.dda3e6ef.chunk.js"
+  },
+  {
+    "revision": "f240feab6c53dfc81b15",
+    "url": "/static/js/1.f240feab.chunk.js"
+  },
+  {
+    "revision": "dda3e6ef8e85d353dfe2",
+    "url": "/static/css/main.1c0e693c.chunk.css"
+  },
+  {
+    "revision": "f240feab6c53dfc81b15",
+    "url": "/static/css/1.6932beb8.chunk.css"
+  },
+  {
+    "revision": "45d7829be399fc7f9c504de43dbebf89",
+    "url": "/index.html"
+  }
+];
--- src/js/scroll-context.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default React.createContext({
-  mainNavFixed: false,
-  menuNavFixed: false,
-  menuNavOffsetTop: 0,
-  navHeight: 0
-});
